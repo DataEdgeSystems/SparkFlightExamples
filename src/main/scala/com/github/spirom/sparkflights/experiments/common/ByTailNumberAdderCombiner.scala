71fb0ff0a36e11c3359f8b250f9500aa1ad3ec2e
Spiro Michaylov <spiro@michaylov.com>
2016-01-30 14:12:48 -0800

    Still trying to wrap an accumulator

--- src/main/scala/com/github/spirom/sparkflights/experiments/common/ByTailNumberAdderCombiner.scala
+++ src/main/scala/com/github/spirom/sparkflights/experiments/common/ByTailNumberAdderCombiner.scala
@@ -1,6 +1,6 @@
 package com.github.spirom.sparkflights.experiments.common
 
-import com.github.spirom.sparkflights.util.{PairAggregateCombiner, MapAccumulator}
+import com.github.spirom.sparkflights.util.{SafeAccumulatorWrapper, PairAggregateCombiner, MapAccumulator}
 
 import scala.reflect.ClassTag
 
@@ -39,3 +39,33 @@ class ByTailNumberAdderCombiner[K:ClassTag]
   override def initial: ByTailNumberAccumulator = new ByTailNumberAccumulator()
 
 }
+
+//------------------------------------------------------------------
+
+//
+// Here we actually wrap our underlying accumulator
+//
+class ByTailNumberWrapper(acc: ByTailNumberAccumulator) extends
+  SafeAccumulatorWrapper[String, ByTailNumberAccumulator](acc)
+{
+
+}
+//
+// SafeAccumulatorWrapper
+//  D = String
+//  IA = ByTailNumberAccumulator <: Accumulator[String, TailAccumulatorWWrapper]]
+//extends Accumulator[D, SafeAccumulatorWrapper[D, IA]] {
+
+/*
+//
+// Aggregate tail numbers by some key
+//
+class ByTailNumberAdderCombiner2[K:ClassTag]
+  extends PairAggregateCombiner[K,String,ByTailNumberWrapper]
+{
+
+  override def initial: ByTailNumberWrapper =
+    new ByTailNumberWrapper(new ByTailNumberAccumulator())
+
+}
+*/
